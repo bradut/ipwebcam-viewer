1a837244a805c19eb8355cc43b19c8113c008447
bradut <BradutDima@yahoo.com>
2019-05-05 20:27:45 -0700

    unit test ImageProviderService

---
+++ IpWebCam3.Tests/Services/ImageServices/ImageFromCacheServiceTests.cs
@@ -0,0 +1,178 @@
+﻿using IpWebCam3.Helpers.Cache;
+using IpWebCam3.Services.ImageServices;
+using NUnit.Framework;
+using System;
+
+namespace IpWebCam3.Tests.Services.ImageServices
+{
+    [TestFixture]
+    public class ImageFromCacheServiceTests
+    {
+
+        [Test]
+        public void Constructor_InvalidNullImageCache_Throws()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ImageFromCacheService(
+                                                                imageCache: null,
+                                                                logger: null,
+                                                                cacheLifeTimeMilliSec: 2500,
+                                                                framesPerSecond: 10));
+        }
+
+        [TestCase(-1, -1)]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(1, 0)]
+        public void Constructor_InvalidParams_Throws(int expiration, int fps)
+        {
+            // Arrange
+            var imageCache = new ImageCache();
+            Assert.Throws<ArgumentException>(() => new ImageFromCacheService(
+                                                                imageCache: imageCache,
+                                                                logger: null,
+                                                                cacheLifeTimeMilliSec: expiration,
+                                                                framesPerSecond: fps));
+        }
+
+        [TestCase(2, 2)]
+        [TestCase(2000, 5)]
+        public void Constructor_ValidParams_HappyPath(int expiration, int fps)
+        {
+            // Arrange
+            var imageCache = new ImageCache();
+
+            // Act
+            var imgCacheSvc = new ImageFromCacheService(imageCache, null, expiration, fps);
+
+            // Assert
+            Assert.IsNotNull(imgCacheSvc);
+        }
+
+
+        [Test]
+        public void GetImageAsByteArray_NoParamsAndImageCacheHasNoData_ReturnsNull()
+        {
+            // Arrange
+            var imageCache = new ImageCache();
+
+            // Act
+            var imgCacheSvc = new ImageFromCacheService(imageCache);
+
+            // Assert
+            Assert.IsNull(imgCacheSvc.GetCurrentImageAsByteArray());
+        }
+
+        [Test]
+        public void GetImageAsByteArray_NoParamsImageCacheHasData_ReturnsNotNull()
+        {
+            // Arrange
+            var imageCache = new ImageCache();
+            imageCache.UpdateImage(new byte[2], 1, DateTime.MaxValue);
+
+            // Act
+            var imgCacheSvc = new ImageFromCacheService(imageCache);
+
+
+            // Assert
+            Assert.IsNotNull(imgCacheSvc.GetCurrentImageAsByteArray());
+        }
+
+
+        private const string StrCacheLastUpdate = "2019-03-31 14:50:00.123";
+
+        [TestCase(1, StrCacheLastUpdate, 2000, 5, true, 99, 1000, true)]   // Success: updaterId != readerId and readerDelay < lifetime
+        [TestCase(1, StrCacheLastUpdate, 2000, 5, false, 99, 1000, false)] // Failure: cache has no data
+        [TestCase(99, StrCacheLastUpdate, 2000, 5, true, 99, 1000, false)] // Failure: updaterId == readerId
+        [TestCase(1, StrCacheLastUpdate, 2000, 5, true, 99, 5000, false)]  // Failure: readerDelay > lifetime
+        public void GetNewImageAsByteArray_WithParamsAndFreshData_ReturnNotNull(int cacheUpdaterUserId, string strCacheLastUpdate, int cacheLifeTime,
+                                                                             int cacheFps, bool cacheHasData,
+                                                                             int cacheReaderUserId, int cacheReaderDelay, bool expectedSuccess)
+        {
+            // Arrange
+            DateTime cacheLastUpdate = DateTime.ParseExact(s: strCacheLastUpdate, format: "yyyy-MM-dd HH:mm:ss.fff", provider: null);
+            DateTime timeWhenCacheIsRead = cacheLastUpdate.AddMilliseconds(cacheReaderDelay);
+
+            byte[] imageBytes = cacheHasData ? new byte[2] : null;
+            ImageCache imageCache = CreateImageCache(imageBytes, cacheUpdaterUserId, cacheLastUpdate);
+            var cachingService = new ImageFromCacheService(imageCache: imageCache, logger: null, cacheLifeTimeMilliSec: cacheLifeTime, framesPerSecond: cacheFps);
+
+            // Act
+            byte[] imgBytes = cachingService.GetNewImageAsByteArray(userId: cacheReaderUserId, timeRequested: timeWhenCacheIsRead);
+            bool resultIsNotNull = imgBytes != null;
+
+            // Assert
+            Assert.AreEqual(resultIsNotNull, expectedSuccess);
+        }
+
+
+        [TestCase(99, StrCacheLastUpdate, 2000, 5, true, 99, 1000, 0)]  // No wait time: updaterId == readerId
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 1000, 0)]  // No wait time: FPS time btw images (200 ms) <  cacheReaderDelay = (1000 ms)
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 225, 175)] // Wait time ( 75 ms) = FPS time btw images (200 ms) - cacheReaderDelay (225 = 1 frame *200 ms/frame + k25 ms)
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 999, 1)]   // Wait time ( 1 ms) = FPS time btw images (200 ms) - cacheReaderDelay (999 = 4 frames *200+199 ms)
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 1000, 0)]  // Wait time ( 0 ms) = FPS time btw images (200 ms) - cacheReaderDelay (1000 = 5*200 ms)
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 1850, 150)] // Wait time ( 150 ms) = FPS time btw images (200 ms) - cacheReaderDelay (1850 = 9 *200 + 50 ms)
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 2050, 150)] // Wait time ( 150 ms) = FPS time btw images (200 ms) - cacheReaderDelay (1850 = 10*200 + 50 ms)
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 990 * 200 + 100, 100)] // Wait time ( 100 ms) = FPS time btw images (200 ms) - cacheReaderDelay (99100 = 990 frames *200 + 100 ms)
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 1000 * 200 * 102 + 50, 0)] // Wait time ( 0 ms) = cacheReaderDelay > 1000 frames * 200 ms/frame
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 0, 0)]     // No wait time:  cacheReaderDelay = 0 ms
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, -1, 0)]    // No wait time:  cacheReaderDelay < 0 ms
+        public void WaitBeforeGettingNextImageTest_AnyParameters_ReturnExpectedResult(int cacheUpdaterUserId, string strCacheLastUpdate, int cacheLifeTime,
+                                                   int cacheFps, bool cacheHasData,
+                                                   int cacheReaderUserId, int cacheReaderDelay, int expectedWaitTimeMilliSec)
+        {
+            // Arrange
+            DateTime cacheLastUpdate = DateTime.ParseExact(s: strCacheLastUpdate, format: "yyyy-MM-dd HH:mm:ss.fff", provider: null);
+            DateTime timeWhenCacheIsRead = cacheLastUpdate.AddMilliseconds(cacheReaderDelay);
+            byte[] imageBytes = cacheHasData ? new byte[2] : null;
+
+            ImageCache imageCache = CreateImageCache(imageBytes, cacheUpdaterUserId, cacheLastUpdate);
+            var cachingService = new ImageFromCacheService(imageCache: imageCache, logger: null, cacheLifeTimeMilliSec: cacheLifeTime, framesPerSecond: cacheFps);
+            int fpsTimeBetweenTwoFramesMilliSec = 1000 / cacheFps;
+
+            // Act
+            int waitTimeMilliSec = cachingService.WaitBeforeGettingNextImage(userId: cacheReaderUserId, timeRequested: timeWhenCacheIsRead);
+
+            // Assert
+            Assert.That(waitTimeMilliSec, Is.EqualTo(expectedWaitTimeMilliSec));
+
+            Assert.That(waitTimeMilliSec, Is.LessThanOrEqualTo(fpsTimeBetweenTwoFramesMilliSec));
+        }
+
+
+
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 1, true)]  // Can update = new date is more recent
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, 0, false)]  // Cannot update = new date is the same
+        [TestCase(10, StrCacheLastUpdate, 2000, 5, true, 99, -1, false)]  // Cannot update = new date is in the past
+        public void UpdateCachedImage_AnyParameters_ReturnExpectedResult(int oldUpdaterUserId, string strCacheLastUpdate, int cacheLifeTime,
+            int cacheFps, bool cacheHasData,
+            int newCacheUpdaterUserId, int newCacheUpdaterDelay, bool expectedSuccess)
+        {
+            // Arrange
+            DateTime cacheLastUpdate = DateTime.ParseExact(s: strCacheLastUpdate, format: "yyyy-MM-dd HH:mm:ss.fff", provider: null);
+            DateTime timeWhenCacheIsRead = cacheLastUpdate.AddMilliseconds(newCacheUpdaterDelay);
+            var oldImageArraySize = 2;
+            byte[] imageBytes = cacheHasData ? new byte[oldImageArraySize] : null;
+
+            ImageCache imageCache = CreateImageCache(imageBytes, oldUpdaterUserId, cacheLastUpdate);
+            var cachingService = new ImageFromCacheService(imageCache: imageCache, logger: null, cacheLifeTimeMilliSec: cacheLifeTime, framesPerSecond: cacheFps);
+            var newImageArraySize = 44;
+
+            // Act
+            cachingService.UpdateCachedImage(new byte[newImageArraySize], newCacheUpdaterUserId, timeWhenCacheIsRead);
+
+            // Assert
+            bool cacheHasBeenUpdated = cachingService.GetCurrentImageAsByteArray().Length == newImageArraySize;
+            Assert.That(cacheHasBeenUpdated, Is.EqualTo(expectedSuccess));
+        }
+
+
+        //Helpers
+        private ImageCache CreateImageCache(byte[] imgBytes, int userId, DateTime dateUpdated)
+        {
+            var imageCache = new ImageCache();
+            imageCache.UpdateImage(imgBytes, userId, dateUpdated);
+
+            return imageCache;
+        }
+    }
+}
---
+++ IpWebCam3.Tests/Services/ImageServices/ImageProviderServiceTests.cs
@@ -0,0 +1,232 @@
+﻿using IpWebCam3.Helpers;
+using IpWebCam3.Helpers.TimeHelpers;
+using IpWebCam3.Models;
+using IpWebCam3.Services.ImageServices;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Drawing;
+using IpWebCam3.Helpers.ImageHelpers;
+using Assert = NUnit.Framework.Assert;
+
+namespace IpWebCam3.Tests.Services.ImageServices
+{
+    [TestFixture]
+    public class ImageProviderServiceTests
+    {
+        private IImageFromCacheService _imageFromCacheService;
+        private IImageFromWebCamService _imageFromWebCamService;
+        private IDateTimeProvider _dateTimeProvider;
+        private MiniLogger _logger;
+        private CacheUpdaterInfo _cacheUpdater;
+        private int _cacheUpdaterExpirationMilliSec;
+
+
+        [SetUp]
+        public void Init()
+        {
+            _imageFromCacheService = Substitute.For<IImageFromCacheService>();
+            _imageFromWebCamService = Substitute.For<IImageFromWebCamService>();
+            _dateTimeProvider = Substitute.For<IDateTimeProvider>();
+            _logger = null;
+            _cacheUpdater = new CacheUpdaterInfo();
+            _cacheUpdaterExpirationMilliSec = 600;
+        }
+
+
+        [TestCase(false, true, true)]
+        [TestCase(true, false, true)]
+        [TestCase(true, true, false)]
+        public void Create_InvalidNullServices_Throws(bool isNullImageFromCacheService, bool isNullImageFromWebCamService, bool isNullDateTimeProvider)
+        {
+            // Arrange:
+            IImageFromCacheService cacheService = isNullImageFromCacheService ? null : _imageFromCacheService;
+            IImageFromWebCamService webCamService = isNullImageFromWebCamService ? null : _imageFromWebCamService;
+            IDateTimeProvider dateTimeService = isNullDateTimeProvider ? null : _dateTimeProvider;
+
+            // Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new ImageProviderService(
+                imageFromCacheService: cacheService,
+                imageFromWebCamService: webCamService,
+                dateTimeProvider: dateTimeService,
+                logger: _logger,
+                cacheUpdaterExpirationMilliSec: 1000,
+                imageErrorLogoUrl: "some_value",
+                lastCacheAccess: DateTime.Now,
+                cacheUpdater: null
+                )
+            );
+        }
+
+        [TestCase(0)]
+        [TestCase(-1)]
+        public void Create_InvalidExpirationValue_Throws(int expirationValueMilliSec)
+        {
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => new ImageProviderService(
+                imageFromCacheService: _imageFromCacheService,
+                imageFromWebCamService: _imageFromWebCamService,
+                dateTimeProvider: _dateTimeProvider,
+                logger: _logger,
+                cacheUpdaterExpirationMilliSec: expirationValueMilliSec,
+                imageErrorLogoUrl: "some_value",
+                lastCacheAccess: DateTime.Now,
+                cacheUpdater: null
+                )
+            );
+        }
+
+
+        [TestCase(1, int.MinValue)]
+        [TestCase(1, int.MaxValue)]
+        [TestCase(1, 0)]
+        [TestCase(1, 10)]
+        public void CanReadImageFromWebCam_SameUserIdAndAnyRequestTime_ReturnTrue(int userId, int requestDelayMilliSec)
+        {
+            // Arrange
+            _dateTimeProvider.DateTimeNow.Returns(new DateTime(2019, 05, 01, 15, 30, 30, 100));
+            _cacheUpdater.Update(userId, _dateTimeProvider.DateTimeNow);
+            IImageProviderService imageProviderService = CreateValidImageProviderService();
+            DateTime requestTime = RequestTime(requestDelayMilliSec);
+
+            // Act
+            bool canReadImageFromWebCam = imageProviderService.CanReadImageFromWebCam(userId, requestTime);
+
+            // Assert
+            Assert.That(canReadImageFromWebCam, Is.True);
+        }
+
+
+        [TestCase(10000, true)]
+        [TestCase(1, true)]
+        [TestCase(0, false)]
+        [TestCase(-1, false)]
+        [TestCase(-10000, false)]
+
+        public void CanReadImageFromWebCam_DifferentUserIdAndCacheUpdaterIsGiven_ReturnExpectedValue(int requestDelayMilliSec, bool expectedCanRead)
+        {
+            // Arrange
+            _dateTimeProvider.DateTimeNow.Returns(new DateTime(2019, 05, 01, 15, 30, 30, 100));
+
+            const int userId = 1;
+            const int cacheUpdaterUserId = userId + 10;
+            _cacheUpdater.Update(userId: cacheUpdaterUserId, lastUpdate: _dateTimeProvider.DateTimeNow);
+
+            IImageProviderService imageProviderService = CreateValidImageProviderService();
+            DateTime requestTime = RequestTime(requestDelayMilliSec + _cacheUpdaterExpirationMilliSec);
+
+            // Act
+            bool canReadImageFromWebCam = imageProviderService.CanReadImageFromWebCam(userId, requestTime);
+
+            // Assert
+            Assert.That(canReadImageFromWebCam, Is.EqualTo(expectedCanRead));
+        }
+
+
+
+        [TestCase(1, 1)]
+        [TestCase(1, 2)]
+        public void GetImageAsByteArray_WhenCannotReadNewImageFromCacheAndCanReadFromWebCam_ReturnImageFromWebCam(int userId, int cacheUpdaterUserId)
+        {
+            // Arrange
+            const string userUtc = "some_UTC_value";
+            _dateTimeProvider.DateTimeNow.Returns(new DateTime(2019, 05, 01, 15, 30, 30, 100));
+            _imageFromCacheService.WaitBeforeGettingNextImage(Arg.Any<int>(), Arg.Any<DateTime>()).Returns(0);
+
+            _imageFromCacheService.GetNewImageAsByteArray(Arg.Any<int>(), Arg.Any<DateTime>()).Returns((byte[])null);
+            _cacheUpdater.Update(userId: cacheUpdaterUserId, lastUpdate: DateTime.MinValue);
+
+            var imageFromWebCam = new Bitmap(1, 1);
+            byte[] imageArrayFromWebCam = ImageHelper.ConvertImageToByteArray(imageFromWebCam);
+            _imageFromWebCamService.GetImage(Arg.Any<string>()).Returns(imageFromWebCam);
+
+            IImageProviderService imageProviderService = CreateValidImageProviderService();
+
+
+            // Act
+            bool canReadImageFromWebCam = imageProviderService.CanReadImageFromWebCam(userId, _dateTimeProvider.DateTimeNow);
+            byte[] imageAsByteArray = imageProviderService.GetImageAsByteArray(userId, userUtc);
+
+            // Assert 
+            Assert.That(canReadImageFromWebCam, Is.True);
+            Assert.That(imageAsByteArray, Is.Not.Null);
+
+            Assert.That(imageAsByteArray.Length, Is.EqualTo(imageArrayFromWebCam.Length));
+
+            Assert.That(_cacheUpdater.LastUpdate, Is.EqualTo(_dateTimeProvider.DateTimeNow));
+            Assert.That(_cacheUpdater.UserId, Is.EqualTo(userId));
+        }
+
+
+        [TestCase(1, 2)]
+        public void GetImageAsByteArray_WhenCannotReadNewImageFromCacheAndCannotReadFromWebCam_ReturnCurrentImageFromCache(int userId, int cacheUpdaterUserId)
+        {
+            // Arrange
+            const string userUtc = "some_UTC_value";
+            _dateTimeProvider.DateTimeNow.Returns(new DateTime(2019, 05, 01, 15, 30, 30, 100));
+            _imageFromCacheService.WaitBeforeGettingNextImage(Arg.Any<int>(), Arg.Any<DateTime>()).Returns(0);
+
+            _imageFromCacheService.GetNewImageAsByteArray(Arg.Any<int>(), Arg.Any<DateTime>()).Returns((byte[]) null);
+            var currentImageArrayFromCache = new byte[20];
+            _imageFromCacheService.GetCurrentImageAsByteArray().Returns(currentImageArrayFromCache);
+
+            _cacheUpdater.Update(userId: cacheUpdaterUserId, lastUpdate: _dateTimeProvider.DateTimeNow);
+
+            var imageFromWebCam = new Bitmap(1, 1);
+            byte[] imageArrayFromWebCam = ImageHelper.ConvertImageToByteArray(imageFromWebCam);
+            _imageFromWebCamService.GetImage(Arg.Any<string>()).Returns(imageFromWebCam);
+
+            IImageProviderService imageProviderService = CreateValidImageProviderService();
+
+
+            // Act
+            bool canReadImageFromWebCam = imageProviderService.CanReadImageFromWebCam(userId, _dateTimeProvider.DateTimeNow);
+            byte[] imageAsByteArray = imageProviderService.GetImageAsByteArray(userId, userUtc);
+
+            // Assert 
+            Assert.That(canReadImageFromWebCam, Is.False);
+            Assert.That(imageAsByteArray, Is.Not.Null);
+
+            Assert.That(imageAsByteArray.Length, Is.EqualTo(currentImageArrayFromCache.Length));
+            Assert.That(imageAsByteArray.Length, Is.Not.EqualTo(imageArrayFromWebCam.Length));
+            
+            Assert.That(_cacheUpdater.UserId, Is.Not.EqualTo(userId));
+        }
+
+
+        // Helpers
+        private IImageProviderService CreateValidImageProviderService()
+        {
+            return new ImageProviderService(
+                imageFromCacheService: _imageFromCacheService,
+                imageFromWebCamService: _imageFromWebCamService,
+                dateTimeProvider: _dateTimeProvider,
+                logger: _logger,
+                cacheUpdaterExpirationMilliSec: _cacheUpdaterExpirationMilliSec,
+                imageErrorLogoUrl: "some_value",
+                lastCacheAccess: DateTime.Now,
+                cacheUpdater: _cacheUpdater
+                );
+        }
+
+        private DateTime RequestTime(int delayMilliSec)
+        {
+            DateTime requestTime;
+            switch (delayMilliSec)
+            {
+                case Int32.MinValue:
+                    requestTime = DateTime.MinValue;
+                    break;
+                case Int32.MaxValue:
+                    requestTime = DateTime.MaxValue;
+                    break;
+                default:
+                    requestTime = _dateTimeProvider.DateTimeNow.AddMilliseconds(delayMilliSec);
+                    break;
+            }
+
+            return requestTime;
+        }
+
+    }
+}
